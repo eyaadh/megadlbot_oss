8d11a296aea7168235e02fa7353c77a035bf1e6c
Mohamed Arham <c4.arham@gmail.com>
2020-09-16 03:29:32 +0500

    - I thought I was smart

--- mega/telegram/__init__.py
+++ mega/telegram/__init__.py
@@ -1,13 +1,19 @@
 from pyrogram import Client
 from mega.common import Common
 
-MegaDLBot = Client(
-    session_name=Common().bot_session,
-    bot_token=Common().bot_api_key,
-    workers=200,
-    workdir=Common().working_dir,
-    if Common().is_env:
+if Common().is_env:
+    MegaDLBot = Client(
+        session_name=Common().bot_session,
+        bot_token=Common().bot_api_key,
+        workers=200,
+        workdir=Common().working_dir,
         plugins=dict(root="mega/telegram/plugins"))
-    else:
+    )
+else:
+    MegaDLBot = Client(
+        session_name=Common().bot_session,
+        bot_token=Common().bot_api_key,
+        workers=200,
+        workdir=Common().working_dir,
         config_file=Common().app_config_file
-)
+    )

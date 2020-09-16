aba80cc8bd265f74701b7100b476d9c5b3b41b77
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-09-16 12:01:43 +0530

    Update __init__.py

--- mega/database/__init__.py
+++ mega/database/__init__.py
@@ -4,6 +4,7 @@ from mega.common import Common
 
 class MegaDB:
     def __init__(self):
-        self.db_client = pymongo.MongoClient(Common().db_url)
-
+        self.db_client = pymongo.MongoClient(
+            f"{Common().db_url}"
+        )
         self.db = self.db_client[Common().db_name]

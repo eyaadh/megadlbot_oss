f635b9eb48bb72e2ca3a7962161092a9180ddf88
eyaadh <eyaadh@gmail.com>
2020-10-09 03:56:42 +0600

    more code style improvements

--- mega/telegram/utils/filters.py
+++ mega/telegram/utils/filters.py
@@ -3,7 +3,8 @@ from pyrogram.types import CallbackQuery
 
 
 def callback_query(args: str, payload=True):
-    """Accepts arg at all times.
+    """
+    Accepts arg at all times.
 
     If payload is True, extract payload from callback and assign to callback.payload
     If payload is False, only check if callback exactly matches argument

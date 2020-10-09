8e12a5555cd29677b1d0c463db1de00c70b093dc
eyaadh <eyaadh@gmail.com>
2020-10-10 00:56:39 +0600

    changes to reflect heroku

--- mega/__main__.py
+++ mega/__main__.py
@@ -3,13 +3,14 @@ from aiohttp import web
 from pyrogram import idle
 from mega.telegram import MegaDLBot
 from mega.webserver import web_server
+from mega.common import Common
 
 
 async def main():
     await MegaDLBot.start()
     runner = web.AppRunner(await web_server())
     await runner.setup()
-    await web.TCPSite(runner, "0.0.0.0", 8080).start()
+    await web.TCPSite(runner, "0.0.0.0", Common().web_port).start()
     await idle()
 
 

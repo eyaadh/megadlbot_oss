c98ac81bba0bc9a82c160561310be28ffbb6072b
ᎫᎪᏟᏦ <69492163+Jack-of-tg@users.noreply.github.com>
2021-06-27 12:34:16 +0530

    better method if get messages causes flood

--- mega/telegram/plugins/common.py
+++ mega/telegram/plugins/common.py
@@ -11,26 +11,7 @@ async def start_message_handler(c: Client, m: Message):
     if len(m.command) > 1:
         if m.command[1].split("-")[0] == 'plf':
             file_id = m.command[1].split("-", 1)[1]
-            file_details = await MegaFiles().get_file_by_file_id(file_id)
-
-            if file_details is not None:
-                file_message = await c.get_messages(
-                    chat_id=file_details['chat_id'],
-                    message_ids=file_details['msg_id']
-                )
-
-                if str(file_details['file_type'].split("/"))[0].lower() == "video":
-                    await m.reply_video(
-                        video=file_message.video.file_id
-                    )
-                elif str(file_details['file_type'].split("/"))[0].lower() == "audio":
-                    await m.reply_audio(
-                        audio=file_message.audio.file_id
-                    )
-                else:
-                    await m.reply_document(
-                        document=file_message.document.file_id
-                    )
+            await m.reply_cached_media(file_id)
     else:
         await m.reply_text(
             text=f"Hello! My name is Megatron {emoji.MAN_BOWING_DARK_SKIN_TONE}"
